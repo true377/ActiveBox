3285614baeb074d567f319d321bd101753e9c2a2
true377 <102599980+true377@users.noreply.github.com>
2022-04-14 18:40:04 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,65 @@
+$(function() {
+
+
+    // Fixed header 
+   let header = $("#header");
+   let intro = $("#intro");
+   let introH = intro.innerHeight();
+   let scrollPos = $(window).scrollTop();
+   let nav = $("#nav");
+   let navToggle = $("#navToggle");
+   
+   checkScroll(scrollPos, introH);
+
+   $(window).on("scroll resize", function() {
+    introH = intro.innerHeight();
+    scrollPos = $(this).scrollTop();
+    checkScroll(scrollPos, introH);
+   });
+
+
+   function checkScroll(scrollPos, introH) {
+    if( scrollPos > introH ) {
+        header.addClass("fixed");
+    } else {
+        header.removeClass("fixed");
+    }
+   }
+
+
+   // smooth scroll 
+   $("[data-scroll]").on("click", function(event) {
+            event.preventDefault(); 
+
+        let elementId = $(this).data('scroll');
+        let elementOffset = $(elementId).offset().top;
+
+            nav.removeClass("show");
+
+            console.log(elementOffset);
+
+    $("html, body").animate({
+            scrollTop: elementOffset - 70
+    }, 700)
+   });
+
+   // nav toggle
+   
+    navToggle.on("click", function(event) {
+        event.preventDefault();
+        nav.toggleClass("show");
+   });
+   
+   // Testimonials (ссылка на библиотеку https://kenwheeler.github.io/slick/)
+        let slider = $("#reviewsSlider");
+
+        slider.slick({
+            infinite: true,  //бесконечность прокрутки
+            slidesToShow: 1, // кол-во показываемых окон
+            slidesToScroll: 1, // сколько окон пролистывать
+            fade: true, //тип анимации прокрутки
+            arrows: false, // показать(скрыть) кнопки(стрелки)
+            dots: true, // показать(скрыть) точки кол-ва слайдеров
+  });
+
+});
